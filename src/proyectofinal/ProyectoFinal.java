d9a0b0f4fbaa00b54b76bab47777d0d2f69a60cd
Gerardo <lgerardoaramburo@gmail.com>
2021-04-20 12:47:54 -0500

    Cambie de clas el metodo main y removi los comentarios

--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectofinal;
 
 /**
  *
- * @author Gerardo
+ * @author Gerardo, Julio y Adriel
  */
 public class ProyectoFinal {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Prestamos().setVisible(true);
+            }
+        });
     }
-    
 }
